893703a826e848f10854747d83f6a11a4ab04de1
Adstart5672 <adlerartsj@gmail.com>
2024-07-02 07:32:05 +0700

    Draw shape

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 int main(int argc, char *argv[]) {
   std::cout << "Hwllo Qoeld" << std::endl;
+  std::cout << "          /|" << std::endl;
+  std::cout << "         / |" << std::endl;
+  std::cout << "        /  |" << std::endl;
+  std::cout << "       /   |" << std::endl;
+  std::cout << "      /____|" << std::endl;
+
   return 0;
 }
