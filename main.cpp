bfd3759781bbe2de1ad27cc88168e3501f76b762
Adstart5672 <adlerartsj@gmail.com>
2024-07-02 17:27:57 +0700

    Add variables

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 int main(int argc, char *argv[]) {
+  int g = 90;
   std::cout << "Hwllo Qoeld" << std::endl;
   std::cout << "          /|" << std::endl;
   std::cout << "         / |" << std::endl;
   std::cout << "        /  |" << std::endl;
   std::cout << "       /   |" << std::endl;
   std::cout << "      /____|" << std::endl;
+  std::cout << g << std::endl;
 
+  g = 10;
+  std::cout << "G =" << g << std::endl;
+  std::string hp = "Hhshs";
+  int h = 20;
+  char pp = 'Z';
+  double fg = 3.1462728291029291209110;
+  bool isGay = false;
+
+  std::cout << hp << std::endl;
+  std::cout << h << std::endl;
+  std::cout << pp << std::endl;
+  std::cout << fg << std::endl;
+  std::cout << isGay << std::endl;
   return 0;
 }
