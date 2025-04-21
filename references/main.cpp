a66eb3b0acc061b0aae1f42073c556dce95bb3b5
AdstardArt <adlerartsj@gmail.com>
2025-04-21 10:13:35 +0000

    hjkhjklhjk

---
+++ references/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    int loooooooong_variable_name = 10;
+    int& l = loooooooong_variable_name;
+    l = 20; // Change the value of the original variable through the reference
+
+    std::cout << "Value: " << l << std::endl; // Output: 20
+    std::cout << "Value of real variable: " << loooooooong_variable_name << std::endl; // Output: 20
+    std::cout << "&l : " << &l << std::endl;
+    std::cout << "sizeof int: " << sizeof(int) << std::endl;
+    std::cout << "sizeof l: " << sizeof(int&) << std::endl; // Output: size of int (usually 4 bytes on 32-bit systems)
+    
+    return 0;
+}
