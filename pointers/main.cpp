dc68287d24d50b887407553f0fec289bd0079b9f
AdstardArt <adlerartsj@gmail.com>
2025-04-26 02:51:36 +0000

    dtgfyj

--- pointers/main.cpp
+++ pointers/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 /**
  * @file main.cpp
@@ -29,7 +30,7 @@
  * 
  * @return int Returns 0 on successful execution.
  */
-int main(int argc, char const *argv[])
+int main()
 {
     int *numbers{};
     double *fractional{};
@@ -71,50 +72,42 @@ int main(int argc, char const *argv[])
     std::cout << "Value pointed to by ptr3: " << *ptr3 << std::endl; // Dereferencing ptr3 to get the value of var3
     std::cout << "Address of ptr3: " << &ptr3 << std::endl; // Address of the pointer itself
 
-    char *p_char_var{nullptr};
-    char char_var{'A'};
-    p_char_var = &char_var; // p_char_var points to char_var
-    std::cout << "Value of char_var: " << char_var << std::endl;
-    std::cout << "Address of char_var: " << &char_var << std::endl;
-    std::cout << "Value pointed to by p_char_var: " << *p_char_var << std::endl;
+    
+    //char *p_char_var{nullptr};
+    //char char_var{'A'};
+    //p_char_var = &char_var; // p_char_var points to char_var
+    //std::cout << "Value of char_var: " << char_var << std::endl;
+    //std::cout << "Address of char_var: " << &char_var << std::endl;
+    //std::cout << "Value pointed to by p_char_var: " << *p_char_var << std::endl; // This line will cause undefined behavior since p_char_var is not initialized
 
-    char char_var2{'B'};
-    p_char_var = &char_var2; // p_char_var now points to char_var2
-    std::cout << "Value of char_var2: " << char_var2 << std::endl;
+    //char char_var2{'B'};
+    //char *p_char_var = nullptr;
+    //p_char_var = &char_var2; // p_char_var now points to char_var2
+    //std::cout << "Value of char_var2: " << char_var2 << std::endl;
+    // std::cout << "Address of char_var2: " << &char_var2 << std::endl;
 
-    char *p_message {"Hello, World!"};
+    char mutable_message[] {"Hello, World!"};
+    char *p_message = mutable_message; // Pointer to the mutable array
     std::cout << "Message: " << p_message << std::endl; // Output: Hello, World!
-    std::cout << "Address of p_message: " << &p_message << std::endl; // Output: Address of the pointer itself
-    std::cout << "Value pointed to by p_message: " << *p_message << std::endl; // Output: H (first character of the string)
 
-    char message[] {"Hello, World!"};
-    message[0] = 'h'; // Changing the first character of the array
-    std::cout << "Modified message: " << message << std::endl; // Output: hello, World!
-
-    //Initialize the pointer with dynamic memory allocation
-    //Dynamically allocate memory at run time and make a pointer point to it
-    int *dynamic_array{nullptr}; // Allocate memory for an array of 5 integers
-    dynamic_array = new int[4]; // Allocate memory for 5 integers
+    // Correct dynamic array allocation
+    int *dynamic_array{nullptr};
+    dynamic_array = new int[5]; // Allocate memory for 5 integers
     dynamic_array[0] = 1;
     dynamic_array[1] = 2;
     dynamic_array[2] = 3;
     dynamic_array[3] = 4;
     dynamic_array[4] = 5; // Initialize the array
-    std::cout << std::endl;
-    for (int i = 0; i < 5; ++i)
-    
+
+    for (int i = 0; i < 5; ++i) // Ensure the loop matches the allocated size
     {
         std::cout << "Value at index " << i << ": " << *(dynamic_array + i) << std::endl;
     }
-   
-    // Deallocate the memory allocated for the dynamic array
-    delete[] dynamic_array; // Use delete[] for arrays
-    std::cout << dynamic_array[1] << std::endl; // Undefined behavior, as the memory has been deallocated
-    std::cout << "Dynamic array pointer after deletion: " << dynamic_array << std::endl; // Still points to the same address, but the memory is deallocated
-    // Set the pointer to nullptr after deletion to avoid dangling pointer
-    dynamic_array = nullptr; // Avoid dangling pointer
-    std::cout << "Dynamic array pointer after setting to nullptr: " << dynamic_array << std::endl; // Now points to nullptr
-    
+
+    // Deallocate memory and avoid dangling pointers
+    delete[] dynamic_array;
+    dynamic_array = nullptr;
+
     {
         int local_var{100};
         int *local_ptr_var = new int; 
@@ -150,12 +143,9 @@ int main(int argc, char const *argv[])
     //    // std::cout << "Value pointed to by ptr3: " << *ptr5 << std::endl; // Undefined behavior
     // This will cause a segmentation fault or access violation error, as the pointer is not initialized to a valid memory location
     int *p_number{new int{62}}; // Dynamically allocate memory for an integer
-    p_number = new int{20}; // Dynamically allocate memory for an integer
-    // Cause memory leak, as the previous memory is not deallocated
-    // To avoid this, always deallocate memory when it is no longer needed
-    
+    delete p_number; // Deallocate the previously allocated memory
+    p_number = new int{20}; // Dynamically allocate memory for another integer
     std::cout << "Value pointed to by p_number: " << *p_number << std::endl; // Output: 20
-    std::cout << "Address of p_number: " << &p_number << std::endl; // Output: Address of the pointer itself
     delete p_number; // Deallocate the memory allocated for the pointer
     p_number = nullptr; // Avoid dangling pointer
 
@@ -225,7 +215,7 @@ int main(int argc, char const *argv[])
     //    delete p_check; // This will not be executed
         p_check = nullptr; // Avoid dangling pointer
     //}
-    delete[] temperatures; nullptr;
+    delete[] temperatures;
     delete[] dynamic_array; // Deallocate the memory allocated for the array of doubles
     delete[] p_students; // Deallocate the memory allocated for the array of integers
     delete[] p_scores; // Deallocate the memory allocated for the array of doubles
@@ -258,5 +248,10 @@ int main(int argc, char const *argv[])
     delete[] dynamic_array2;
     dynamic_array2 = nullptr;
     
+    // Smart pointers
+    std::unique_ptr<int> smart_ptr1(new int(100)); // Unique pointer to an integer
+    std::cout << "Value pointed to by smart_ptr1: " << *smart_ptr1 << std::endl; // Output: 100
+    std::cout << "Address of smart_ptr1: " << &smart_ptr1 << std::endl; // Output: address of the unique pointer itself
+    // Don't need to delete smart_ptr1, it will be automatically deleted when it goes out of scope
     return 0;
 }
--- references/main.cpp
+++ references/main.cpp
@@ -12,5 +12,16 @@ int main() {
     std::cout << "sizeof int: " << sizeof(int) << std::endl;
     std::cout << "sizeof l: " << sizeof(int&) << std::endl; // Output: size of int (usually 4 bytes on 32-bit systems)
      
+    // Pointers vs References
+    double double_variable = 3.14;
+    double& ref_double_variable = double_variable; // Reference to double_variable
+    double* ptr_double_variable = &double_variable; // Pointer to double_variable
+
+
+    std::cout << "Value of double_variable: " << double_variable << std::endl;
+    std::cout << "Value through reference: " << ref_double_variable << std::endl;
+    std::cout << "Value through pointer: " << *ptr_double_variable << std::endl;
+    std::cout << "ptr_double_value : " << ptr_double_variable << std::endl;
+
     return 0;
 }
