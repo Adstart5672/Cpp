341e2e3208657f7f48cc689a8af8781af6168c6c
AdstardArt <adlerartsj@gmail.com>
2025-05-11 10:00:16 +0000

    sjwkkwkwksksk

--- stringmanipulation/main.cpp
+++ stringmanipulation/main.cpp
@@ -8,5 +8,11 @@ int main()
     std::cout << "Length: " << str.length() << std::endl;
     str[0] = 'h'; // Change first character to lowercase
     std::cout << "Modified string: " << str << std::endl;
+
+    // Check if character is alphanumeric
+    std::cout << std::endl;
+    std::cout << "std::isalnum('A'): " << std::isalnum('A') << std::endl;
+    std::cout << "std::isalnum('^'): " << std::isalnum('^') << std::endl;
+    std::cout << "std::isalnum('1'): " << std::isalnum('1') << std::endl;
     return 0;
 }
